40bfb3241e166bb11258d9f301c8d1e4258fb40c
oliLamRou <parties01smacks@icloud.com>
2025-06-16 21:54:36 +0900

    zsh

---
+++ zsh-custom/aliases.zsh
@@ -0,0 +1,15 @@
+#term
+export TERM=xterm-256color
+
+#Homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
+#nvim
+alias vi="nvim"
+
+#Server
+alias sauron="ssh admin@100.107.7.96"
+
+#Dev
+alias dev="cd ~/Developer/"
