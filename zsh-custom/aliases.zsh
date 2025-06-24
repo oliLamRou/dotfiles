86443afdbbec9f173defc3664ddf88360719a700
oliLamRou <parties01smacks@icloud.com>
2025-06-24 17:15:35 +0900

    /etc

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -4,6 +4,7 @@ export TERM=xterm-256color
 #Homebrew
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/opt/postgresql@16/bin:$PATH"
 
 #nvim
 alias vi="nvim"
