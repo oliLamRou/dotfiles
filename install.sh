b3ded4d494fff1ed166091bd3324a600443c4be1
oliLamRou <parties01smacks@icloud.com>
2026-01-11 12:14:07 +0900

    simplify nvim

---
+++ install.sh
@@ -0,0 +1,8 @@
+#Install
+git clone --depth=1 https://github.com/stevearc/conform.nvim.git ~/.config/nvim/pack/plugins/start/conform.nvim
+git clone --depth=1 https://github.com/stevearc/oil.nvim.git ~/.config/nvim/pack/plugins/start/oil.nvim
+git clone https://github.com/catppuccin/nvim ~/.config/nvim/pack/plugins/start/catppuccin
+git clone https://github.com/echasnovski/mini.nvim ~/.config/nvim/pack/plugins/start/mini.nvim
+
+#SymLink
+#ln -s 
--- package.py
+++
@@ -1,4 +0,0 @@
-name = "dotfiles"
-version = "0.1.0"
-
-build_command = False
