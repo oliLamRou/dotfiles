814f059bbd9248b277565320074f2dbcf8b444fc
oliLamRou <parties01smacks@icloud.com>
2026-01-11 13:29:29 +0900

    switch to starshrip

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-#Install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#Starship
+brew install starship
+
+#Zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+
+#Neo Vim
+mkdir -p ~/.config/nvim/pack/plugins/start
 git clone --depth=1 https://github.com/stevearc/conform.nvim.git ~/.config/nvim/pack/plugins/start/conform.nvim
 git clone --depth=1 https://github.com/stevearc/oil.nvim.git ~/.config/nvim/pack/plugins/start/oil.nvim
-git clone https://github.com/catppuccin/nvim ~/.config/nvim/pack/plugins/start/catppuccin
-git clone https://github.com/echasnovski/mini.nvim ~/.config/nvim/pack/plugins/start/mini.nvim
+git clone --depth=1 https://github.com/catppuccin/nvim ~/.config/nvim/pack/plugins/start/catppuccin
+git clone --depth=1 https://github.com/echasnovski/mini.nvim ~/.config/nvim/pack/plugins/start/mini.nvim
 
 #SymLink
 ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc 
 ln -sf $HOME/dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
+
+. ~/zshrc
