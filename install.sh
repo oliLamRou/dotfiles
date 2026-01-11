6e4ea1111b9c74d27826d7301578bd23da95d3e8
oliLamRou <parties01smacks@icloud.com>
2026-01-11 22:24:06 +0900

    add bash

--- install.sh
+++ install.sh
@@ -5,34 +5,40 @@ brew install starship
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 
+#Bash
+# git clone https://github.com/akinomyoga/ble.sh.git ~/.ble.sh
+#.bashrc
+# Ble.sh (autosuggestions + syntax highlighting)
+# [[ $- == *i* ]] && source ~/.ble.sh/out/ble.sh
+
 #Neo Vim
 mkdir -p ~/.config/nvim/pack/plugins/start
 
 git clone --depth=1 https://github.com/stevearc/conform.nvim.git \
   ~/.config/nvim/pack/plugins/start/conform.nvim
+
 git clone --depth=1 https://github.com/stevearc/oil.nvim.git \
   ~/.config/nvim/pack/plugins/start/oil.nvim
+
 git clone --depth=1 https://github.com/catppuccin/nvim \
   ~/.config/nvim/pack/plugins/start/catppuccin
+
 git clone --depth=1 https://github.com/echasnovski/mini.nvim \
   ~/.config/nvim/pack/plugins/start/mini.nvim
 
 # Mason
-git clone --depth=1 https://github.com/williamboman/mason.nvim \
-  ~/.config/nvim/pack/plugins/start/mason.nvim
-git clone --depth=1 https://github.com/williamboman/mason-lspconfig.nvim \
-  ~/.config/nvim/pack/plugins/start/mason-lspconfig.nvim
-git clone --depth=1 https://github.com/neovim/nvim-lspconfig \
-  ~/.config/nvim/pack/plugins/start/nvim-lspconfig
-
-git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter \
-  ~/.config/nvim/pack/plugins/start/nvim-treesitter
+# git clone --depth=1 https://github.com/williamboman/mason.nvim \
+#   ~/.config/nvim/pack/plugins/start/mason.nvim
+# git clone --depth=1 https://github.com/williamboman/mason-lspconfig.nvim \
+#   ~/.config/nvim/pack/plugins/start/mason-lspconfig.nvim
+# git clone --depth=1 https://github.com/neovim/nvim-lspconfig \
+#   ~/.config/nvim/pack/plugins/start/nvim-lspconfig
+# git clone --depth=1 https://github.com/nvim-treesitter/nvim-treesitter \
+#   ~/.config/nvim/pack/plugins/start/nvim-treesitter
 
 #SymLink
 ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc 
 ln -sf $HOME/dotfiles/nvim/init.lua $HOME/.config/nvim/init.lua
-
-. ~/zshrc
 
 # Python Formatter
 pip install --break-system-packages black isort
